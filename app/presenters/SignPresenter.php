e8a9a10267d74de9ebf2222c0ecceacb22f59bdc
freeroman <romanpechal@Romans-MacBook-Air.local>
2014-04-26 21:53:43 +0200

    begin

---
+++ app/presenters/SignPresenter.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+
+/**
+ * Sign in/out presenters.
+ */
+class SignPresenter extends BasePresenter
+{
+
+
+	/**
+	 * Sign-in form factory.
+	 * @return Nette\Application\UI\Form
+	 */
+	protected function createComponentSignInForm()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->addText('username', 'Username:')
+			->setRequired('Please enter your username.');
+
+		$form->addPassword('password', 'Password:')
+			->setRequired('Please enter your password.');
+
+		$form->addCheckbox('remember', 'Keep me signed in');
+
+		$form->addSubmit('send', 'Sign in');
+                
+                $renderer = $form->getRenderer();
+                $renderer->wrappers['controls']['container'] = NULL;
+                $renderer->wrappers['pair']['container'] = 'div class=form-group';
+                $renderer->wrappers['pair']['.error'] = 'has-error';
+                $renderer->wrappers['control']['container'] = 'div class=col-sm-9';
+                $renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
+                $renderer->wrappers['control']['description'] = 'span class=help-block';
+                $renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+
+                // make form and controls compatible with Twitter Bootstrap
+                $form->getElementPrototype()->class('form-horizontal');
+
+                foreach ($form->getControls() as $control) {
+                        if ($control instanceof Controls\Button) {
+                                //$control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-default' : 'btn btn-default');
+                                //$usedPrimary = TRUE;
+                            $control->getControlPrototype()->addClass('btn btn-default');
+
+                        } elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox) {
+                                $control->getControlPrototype()->addClass('form-control');
+
+                        } elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList) {
+                                $control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
+                        }
+                }
+
+		// call method signInFormSucceeded() on success
+		$form->onSuccess[] = $this->signInFormSucceeded;
+		return $form;
+	}
+
+
+	public function signInFormSucceeded($form)
+	{
+		$values = $form->getValues();
+                //$this->getUser()->getAuthenticator()->add('roman', 'test');
+
+		if ($values->remember) {
+			$this->getUser()->setExpiration('14 days', FALSE);
+		} else {
+			$this->getUser()->setExpiration('20 minutes', TRUE);
+		}
+
+		try {
+			$this->getUser()->login($values->username, $values->password);
+			$this->redirect('Homepage:');
+
+		} catch (Nette\Security\AuthenticationException $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+
+
+	public function actionOut()
+	{
+		$this->getUser()->logout();
+		$this->flashMessage('You have been signed out.');
+		$this->redirect('in');
+	}
+
+}
---
+++ temp/cache/_Nette.FileTemplate/_templates._layout.latte-1a08a4d765a7cc8e7d3dd3668eca4898.php
@@ -0,0 +1,140 @@
+<?php //netteCache[01]000389a:2:{s:4:"time";s:21:"0.21802900 1398541647";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:75:"/Users/romanpechal/Documents/GIT/social_network/app/templates/@layout.latte";i:2;i:1398537992;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:22:"released on 2014-03-17";}}}?><?php
+
+// source file: /Users/romanpechal/Documents/GIT/social_network/app/templates/@layout.latte
+
+?><?php
+// prolog Nette\Latte\Macros\CoreMacros
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 's75ie55gjs')
+;
+// prolog Nette\Latte\Macros\UIMacros
+//
+// block title
+//
+if (!function_exists($_l->blocks['title'][] = '_lbf0476f1a37_title')) { function _lbf0476f1a37_title($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+?>Nette Application Skeleton<?php
+}}
+
+//
+// block head
+//
+if (!function_exists($_l->blocks['head'][] = '_lbd10d541cad_head')) { function _lbd10d541cad_head($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+;
+}}
+
+//
+// block scripts
+//
+if (!function_exists($_l->blocks['scripts'][] = '_lbe4caab73d0_scripts')) { function _lbe4caab73d0_scripts($_l, $_args) { foreach ($_args as $__k => $__v) $$__k = $__v
+?>        <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="<?php echo htmlSpecialChars(Nette\Templating\Helpers::safeUrl($basePath)) ?>/js/jquery.js"></script>
+	<script src="<?php echo htmlSpecialChars(Nette\Templating\Helpers::safeUrl($basePath)) ?>/js/netteForms.js"></script>
+	<script src="<?php echo htmlSpecialChars(Nette\Templating\Helpers::safeUrl($basePath)) ?>/js/main.js"></script>
+<?php
+}}
+
+//
+// end of blocks
+//
+
+// template extending and snippets support
+
+$_l->extends = empty($template->_extended) && isset($_control) && $_control instanceof Nette\Application\UI\Presenter ? $_control->findLayoutTemplateFile() : NULL; $template->_extended = $_extended = TRUE;
+
+
+if ($_l->extends) {
+	ob_start();
+
+} elseif (!empty($_control->snippetMode)) {
+	return Nette\Latte\Macros\UIMacros::renderSnippets($_control, $_l, get_defined_vars());
+}
+
+//
+// main template
+//
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+<?php if (isset($robots)) { ?>	<meta name="robots" content="<?php echo htmlSpecialChars($robots) ?>">
+<?php } ?>
+
+	<title><?php if ($_l->extends) { ob_end_clean(); return Nette\Latte\Macros\CoreMacros::includeTemplate($_l->extends, get_defined_vars(), $template)->render(); }
+ob_start(); call_user_func(reset($_l->blocks['title']), $_l, get_defined_vars()); echo $template->upper($template->striptags(ob_get_clean()))  ?></title>
+        
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" media="screen,projection,tv" href="<?php echo htmlSpecialChars(Nette\Templating\Helpers::safeUrl($basePath)) ?>/css/screen.css">
+	<!--<link rel="stylesheet" media="print" href="<?php echo Nette\Templating\Helpers::escapeHtmlComment($basePath) ?>/css/print.css">-->
+	<link rel="shortcut icon" href="<?php echo htmlSpecialChars(Nette\Templating\Helpers::safeUrl($basePath)) ?>/favicon.ico">
+	<?php call_user_func(reset($_l->blocks['head']), $_l, get_defined_vars())  ?>
+
+</head>
+
+<body>
+	<script> document.documentElement.className+=' js' </script>
+        <!-- Fixed navbar -->
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="<?php echo htmlSpecialChars($_control->link("homepage:default")) ?>">Social network</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo htmlSpecialChars($_control->link("homepage:default")) ?>">Home</a></li>
+                <li><a href="<?php echo htmlSpecialChars($_control->link("sign:in")) ?>">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo htmlSpecialChars($_control->link("sign:in")) ?>">Sign in</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div>
+        </div>
+
+        <div class="container">
+
+          <!-- Main component for a primary marketing message or call to action -->
+          <div class="jumbotron">
+<?php $iterations = 0; foreach ($flashes as $flash) { ?>              <div class="flash <?php echo htmlSpecialChars($flash->type) ?>
+"><?php echo Nette\Templating\Helpers::escapeHtml($flash->message, ENT_NOQUOTES) ?></div>
+<?php $iterations++; } ?>
+
+<?php Nette\Latte\Macros\UIMacros::callBlock($_l, 'content', $template->getParameters()) ?>
+            <!--
+            <h1>Navbar example</h1>
+            <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+            <p>To see the difference between static and fixed top navbars, just scroll.</p>
+            <p>
+              <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+            </p>
+            -->
+          </div>
+
+        </div> <!-- /container -->	
+
+<?php call_user_func(reset($_l->blocks['scripts']), $_l, get_defined_vars())  ?>
+</body>
+</html>
