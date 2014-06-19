c11964d4e2123eada426c3162a739e0def54b45b
freeroman <roman@pechal.net>
2014-06-19 14:43:59 +0200

    db

--- social_network.sql
+++ social_network.sql
@@ -1,32 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 4.0.6
--- http://www.phpmyadmin.net
---
--- Počítač: localhost
--- Vygenerováno: Ned 27. dub 2014, 20:51
--- Verze serveru: 5.5.33
--- Verze PHP: 5.5.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Databáze: `social_network`
---
+CREATE DATABASE IF NOT EXISTS `social_network` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `social_network`;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `albums`
---
-
-CREATE TABLE `albums` (
+CREATE TABLE IF NOT EXISTS `albums` (
   `id_albums` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `description` varchar(1000) DEFAULT NULL,
@@ -40,13 +23,7 @@ CREATE TABLE `albums` (
   KEY `id_walls` (`id_walls`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `employees`
---
-
-CREATE TABLE `employees` (
+CREATE TABLE IF NOT EXISTS `employees` (
   `id_employees` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
   `first_name` varchar(50) NOT NULL,
@@ -60,17 +37,12 @@ CREATE TABLE `employees` (
   `visible_job_title` tinyint(1) NOT NULL DEFAULT '1',
   `password` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_employees`),
   KEY `id_walls` (`id_walls`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `events`
---
-
-CREATE TABLE `events` (
+CREATE TABLE IF NOT EXISTS `events` (
   `id_events` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `place` varchar(255) DEFAULT NULL,
@@ -83,15 +55,9 @@ CREATE TABLE `events` (
   PRIMARY KEY (`id_events`),
   KEY `id_employees` (`id_employees`),
   KEY `id_walls` (`id_walls`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `events_employees`
---
-
-CREATE TABLE `events_employees` (
+CREATE TABLE IF NOT EXISTS `events_employees` (
   `id_events` int(11) NOT NULL,
   `id_employees` int(11) NOT NULL,
   `attendance` tinyint(1) DEFAULT NULL,
@@ -101,37 +67,25 @@ CREATE TABLE `events_employees` (
   KEY `id_employees` (`id_employees`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `files`
---
-
-CREATE TABLE `files` (
+CREATE TABLE IF NOT EXISTS `files` (
   `id_files` int(11) NOT NULL AUTO_INCREMENT,
   `file` varchar(255) NOT NULL,
   `id_employees` int(11) NOT NULL,
-  `id_sharing_type` int(11) NOT NULL,
+  `id_sharing_types` int(11) NOT NULL,
   `created_dt` datetime NOT NULL,
   `id_albums` int(11) DEFAULT NULL,
   `type` char(1) NOT NULL,
   `visible_from` datetime NOT NULL,
   `visible_to` datetime NOT NULL,
-  `id_walls` int(11) NOT NULL,
+  `id_messages` int(11) NOT NULL,
   PRIMARY KEY (`id_files`),
   KEY `id_employees` (`id_employees`),
-  KEY `id_sharing_type` (`id_sharing_type`),
   KEY `id_albums` (`id_albums`),
-  KEY `id_walls` (`id_walls`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  KEY `id_messages` (`id_messages`),
+  KEY `id_sharing_types` (`id_sharing_types`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `groups`
---
-
-CREATE TABLE `groups` (
+CREATE TABLE IF NOT EXISTS `groups` (
   `id_groups` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `id_employees` int(11) NOT NULL,
@@ -140,15 +94,17 @@ CREATE TABLE `groups` (
   PRIMARY KEY (`id_groups`),
   KEY `id_employees` (`id_employees`),
   KEY `id_walls` (`id_walls`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `groups_employees` (
+  `id_groups` int(11) NOT NULL,
+  `id_employees` int(11) NOT NULL,
+  PRIMARY KEY (`id_groups`,`id_employees`),
+  KEY `id_groups` (`id_groups`),
+  KEY `id_employees` (`id_employees`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Struktura tabulky `messages`
---
-
-CREATE TABLE `messages` (
+CREATE TABLE IF NOT EXISTS `messages` (
   `id_messages` int(11) NOT NULL AUTO_INCREMENT,
   `id_walls` int(11) NOT NULL,
   `id_employees` int(11) NOT NULL,
@@ -161,15 +117,9 @@ CREATE TABLE `messages` (
   KEY `id_employees` (`id_employees`),
   KEY `id_sharing_types` (`id_sharing_types`),
   KEY `id_walls` (`id_walls`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=84 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `relationships`
---
-
-CREATE TABLE `relationships` (
+CREATE TABLE IF NOT EXISTS `relationships` (
   `id_relationships` int(11) NOT NULL AUTO_INCREMENT,
   `id_employees1` int(11) NOT NULL,
   `id_employees2` int(11) NOT NULL,
@@ -181,98 +131,60 @@ CREATE TABLE `relationships` (
   PRIMARY KEY (`id_relationships`),
   KEY `id_employees1` (`id_employees1`),
   KEY `id_employees2` (`id_employees2`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=44 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `sharing_types`
---
-
-CREATE TABLE `sharing_types` (
+CREATE TABLE IF NOT EXISTS `sharing_types` (
   `id_sharing_types` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id_sharing_types`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
--- --------------------------------------------------------
-
---
--- Struktura tabulky `walls`
---
-
-CREATE TABLE `walls` (
+CREATE TABLE IF NOT EXISTS `walls` (
   `id_walls` int(11) NOT NULL AUTO_INCREMENT,
   `id_sharing_types` int(11) NOT NULL,
   PRIMARY KEY (`id_walls`),
   KEY `id_sharing_types` (`id_sharing_types`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=41 ;
 
---
--- Omezení pro exportované tabulky
---
 
---
--- Omezení pro tabulku `albums`
---
 ALTER TABLE `albums`
   ADD CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`),
   ADD CONSTRAINT `albums_ibfk_2` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`);
 
---
--- Omezení pro tabulku `employees`
---
 ALTER TABLE `employees`
   ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`);
 
---
--- Omezení pro tabulku `events`
---
 ALTER TABLE `events`
   ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`),
   ADD CONSTRAINT `events_ibfk_2` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`);
 
---
--- Omezení pro tabulku `events_employees`
---
 ALTER TABLE `events_employees`
   ADD CONSTRAINT `events_employees_ibfk_1` FOREIGN KEY (`id_events`) REFERENCES `events` (`id_events`),
   ADD CONSTRAINT `events_employees_ibfk_2` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`);
 
---
--- Omezení pro tabulku `files`
---
 ALTER TABLE `files`
   ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`),
-  ADD CONSTRAINT `files_ibfk_2` FOREIGN KEY (`id_sharing_type`) REFERENCES `sharing_types` (`id_sharing_types`),
   ADD CONSTRAINT `files_ibfk_3` FOREIGN KEY (`id_albums`) REFERENCES `albums` (`id_albums`),
-  ADD CONSTRAINT `files_ibfk_4` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`);
+  ADD CONSTRAINT `files_ibfk_4` FOREIGN KEY (`id_messages`) REFERENCES `messages` (`id_messages`),
+  ADD CONSTRAINT `files_ibfk_5` FOREIGN KEY (`id_sharing_types`) REFERENCES `sharing_types` (`id_sharing_types`);
 
---
--- Omezení pro tabulku `groups`
---
 ALTER TABLE `groups`
   ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`),
   ADD CONSTRAINT `groups_ibfk_2` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`);
 
---
--- Omezení pro tabulku `messages`
---
-ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_3` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`),
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`),
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`id_sharing_types`) REFERENCES `sharing_types` (`id_sharing_types`);
+ALTER TABLE `groups_employees`
+  ADD CONSTRAINT `groups_employees_ibfk_1` FOREIGN KEY (`id_groups`) REFERENCES `groups` (`id_groups`),
+  ADD CONSTRAINT `groups_employees_ibfk_2` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`);
 
---
--- Omezení pro tabulku `relationships`
---
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`id_employees`) REFERENCES `employees` (`id_employees`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`id_sharing_types`) REFERENCES `sharing_types` (`id_sharing_types`),
+  ADD CONSTRAINT `messages_ibfk_3` FOREIGN KEY (`id_walls`) REFERENCES `walls` (`id_walls`);
+
 ALTER TABLE `relationships`
   ADD CONSTRAINT `relationships_ibfk_1` FOREIGN KEY (`id_employees1`) REFERENCES `employees` (`id_employees`),
   ADD CONSTRAINT `relationships_ibfk_2` FOREIGN KEY (`id_employees2`) REFERENCES `employees` (`id_employees`);
 
---
--- Omezení pro tabulku `walls`
---
 ALTER TABLE `walls`
   ADD CONSTRAINT `walls_ibfk_1` FOREIGN KEY (`id_sharing_types`) REFERENCES `sharing_types` (`id_sharing_types`);
 
